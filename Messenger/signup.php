76610eba91f5ad615eac6ef8d028bb33ff65630e
Asyran <1221108650@student.mmu.edu.my>
2024-05-28 00:33:26 +0800

    collect user input

--- Messenger/signup.php
+++ Messenger/signup.php
@@ -46,17 +46,17 @@
     }
 
 
-    input[type=text],input[type=password],input[type=submit] {
+    input[type=text],input[type=password],input[type=button] {
 
         padding: 10px;
         margin: 10px;
-        width: 98%;
+        width: 100%;
         border-radius: 5px;
         border: solid 1px grey;
 
     }
 
-    input[type=submit] {
+    input[type=button] {
 
         width: 103%;
         cursor: pointer;
@@ -78,19 +78,19 @@
 
        <div id="header">
         Messenger
-        <div style="font-size: 20px; margin: 10px;" >Login</div>
+        <div style="font-size: 20px; margin: 10px;">Sign Up</div>
        </div>
-       <form>
+       <form id="myform">
         <input type="text" name="username" placeholder="Username"><br>
         <input type="text" name="email" placeholder="Email"><br>
         <div style="padding: 10px;">
           <br>Gender:<br>
-          <input type="radio" name="gender"> Male<br>
-          <input type="radio" name="gender"> Female<br>
+          <input type="radio" value="Male" name="gender"> Male<br>
+          <input type="radio" value="Female" name="gender"> Female<br>
         </div>
         <input type="password" name="password" placeholder="Password"><br>
         <input type="password" name="password2" placeholder="Retype Password"><br>
-        <input type="submit" value="Sign up"><br>
+        <input type="button" value="Sign up" id="signup_button"><br>
 
        </form>
     </div>
@@ -104,24 +104,70 @@
         return document.getElementById(element);
     }
 
-    var label = _("label_chat")
-    label.addEventListener("click",function(){
-        
-        var inner_pannel = _("inner_left_pannel");
+    var signup_button = _("signup_button");
+    signup_button.addEventListener("click",collect_data);
 
-        var ajax = new XMLHttpRequest();
-        ajax.onload = function(){
+    function collect_data(){
 
-            if(ajax.status == 200 || ajax.readyState == 4){
+        var myform = _("myform");
+        var inputs = myform.getElementsByTagName("INPUT");
 
-                inner_pannel.innerHTML = ajax.responseText;
+        var data = {};
+        for (var i = inputs.length - 1; i >= 0; i--) {
 
+            var key = inputs[i].name;
+
+            switch(key){
+                
+                case "username":
+                    data.username = inputs[i].value;    
+                    break;
+
+                case "email":
+                    data.email = inputs[i].value;    
+                    break;
+
+                case "gender":
+                    if(inputs[i].checked){
+                        data.gender = inputs[i].value;
+                    }   
+                    break;
+
+                case "password":
+                    data.password = inputs[i].value;    
+                    break;
+
+                case "password2":
+                    data.password2 = inputs[i].value;    
+                    break;
+                
             }
         }
 
-        ajax.open("POST","file.php",true);
-        ajax.send();
+        send_data(data,"signup");
+         
+    }
+    
+    function send_data(data,type){
 
-    })
+        var xml = new XMLHttpRequest();
+
+        xml.onload = function(){
+
+            if(xml.readyState == 4 || xml.status == 200){
+
+                alert(xml.responseText);
+
+            }
+        }    
+           
+        data.data_type = type;
+        var data_string = JSON.stringify(data);
+
+        xml.open("POST","api.php",true);
+        xml.send(data_string);
+
+        
+    }
 
 </script>
