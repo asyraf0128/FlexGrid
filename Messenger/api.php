5b6c3f97c4403a156a7fe616c3cb2d4c18117482
Asyran <1221108650@student.mmu.edu.my>
2024-05-31 02:30:02 +0800

    delete testing file

--- Messenger/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("classes/autoload.php");
-$DB = new Database();
-
-$DATA_RAW = file_get_contents("php://input");
-$DATA_OBJ = json_decode($data,true);
-
-//process data
--- Messenger/classes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('DBUSER', "root");
-define('DBPASS', "");
-define('DBNAME', "messenger_db");
-define('DBHOST', "LocalHost");
