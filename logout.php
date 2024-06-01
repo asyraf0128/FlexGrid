5ff79b09b6e4c8395376d2a9c20ca81e009e7949
Amir Asyraf <emailforasyraf@gmail.com>
2024-06-01 20:38:07 +0800

    logout.php

---
+++ logout.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'header.php';
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+
+// This will destroy the session, and not just the session data.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+//Destroy the session.
+session_destroy();
+
+// Redirect to login page or home page
+header("Location: index.php");
+exit();
+?>
